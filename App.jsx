23f7e78104dfdec366d45f860c1662ef411c664b
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2024-10-21 19:17:52 +0800

    Partially Create Title
    inner shadow not yet styled

--- App.jsx
+++ App.jsx
@@ -1,21 +1,48 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   const sa = "sd";
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Cal Talk</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#FFEBCD",
     alignItems: "center",
+    justifyContent: "start",
+  },
+
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+    borderRadius: 20,
+  },
+
+  titleContainer: {
+    backgroundColor: "#664229",
+    flex: 1,
+    borderRadius: 20,
     justifyContent: "center",
+    marginTop: 32,
+    width: 260,
+    maxHeight: 56,
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.31,
+    shadowRadius: 1.84,
+
+    // Android Shadow
+    elevation: 4,
   },
 });
